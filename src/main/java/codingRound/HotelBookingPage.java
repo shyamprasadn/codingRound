24f977c9478e5ce1fcbf126e33550ee6b97f5b87
Shyam <Shyam@192.168.0.100>
2018-05-04 22:49:59 +0530

    Adding BaseLaunchTest class to facilitate configurations in before suite

---
+++ src/main/java/codingRound/HotelBookingPage.java
@@ -0,0 +1,43 @@
+package codingRound;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class HotelBookingPage {
+
+private WebDriver driver;
+	
+	@FindBy(linkText = "Hotels")
+    private WebElement hotelLink;
+
+    @FindBy(id = "Tags")
+    private WebElement localityTextBox;
+
+    @FindBy(id = "SearchHotelsButton")
+    private WebElement searchButton;
+
+    @FindBy(id = "travellersOnhome")
+    private WebElement travellerSelection;
+
+    public HotelBookingPage(WebDriver driver)
+    {
+    	this.driver = driver;
+    	PageFactory.initElements(driver, this);
+    }
+    
+    public void openUrlandClickHotel()
+    {
+    	  driver.get("https://www.cleartrip.com/");
+          hotelLink.click();
+    }
+    
+    public void setPlaceandSelectRooms()
+    {
+    	localityTextBox.sendKeys("Indiranagar, Bangalore");
+        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
+        searchButton.click();
+    }
+}
